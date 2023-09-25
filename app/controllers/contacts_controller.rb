e7e3641a37318c92f79cad89177c81d0aeab84a5
Matt Pike <mattp91@gmail.com>
2023-09-25 13:04:01 -0400

    Styling touches / Finished dropdown filter

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,23 @@
 class ContactsController < InheritedResources::Base
 
+  def index
+    sort_by = params[:sort_by]
+
+    if sort_by == 'name'
+      @contacts = Contact.order(:name)
+    elsif sort_by == 'email'
+      @contacts = Contact.order(:email)
+    elsif sort_by == 'birthday'
+      @contacts = Contact.order(:birthday)
+    else
+      @contacts = Contact.all
+    end
+  end
+
   private
 
-    def contact_params
-      params.require(:contact).permit(:name, :email, :birthday)
-    end
+  def contact_params
+    params.require(:contact).permit(:name, :email, :birthday)
+  end
 
 end
