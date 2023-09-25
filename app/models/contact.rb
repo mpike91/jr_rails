d9dab13d5eed3e33225840a29aea62a71cef9fbb
Matt Pike <mattp91@gmail.com>
2023-09-25 11:07:47 -0400

    Restyled Contact pages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
+  validates :name, :email, :birthday, presence: true
+
   def self.ransackable_attributes(auth_object = nil)
     ["birthday", "created_at", "email", "id", "name", "updated_at"]
   end
